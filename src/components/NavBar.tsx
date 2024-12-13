3c273baa17fbe4cf55fbb333c4d04a1bfaf270fb
Eric Dirr <edirr92@aol.com>
2024-12-13 13:16:09 -0500

    nav bar logo size increase

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 const NavBar: React.FC = () => {
   return (
     <nav className="bg-white shadow-lg">
-      <div className="flex items-center justify-between px-4 py-3">
+      <div className="flex items-center justify-between">
         <div className="flex-shrink-0">
           <Link href="/">
             <img
               src="/assets/logo-black-text.png"
               alt="Stellar Learn Logo"
-              className="h-16 w-auto object-contain"
+              className="h-20 w-auto object-contain"
             />
           </Link>
         </div>
